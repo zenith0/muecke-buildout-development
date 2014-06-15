31faaf0bf3fc2d893ae0e09050bc736c772eabe9
stefan <stefan@debian-pc.home>
2014-06-15 18:20:01 +0200

    added not staged files

--- ecomstore/catalog/admin.py
+++ ecomstore/catalog/admin.py
@@ -1,1 +1,38 @@
 __author__ = 'stefan'
+from django.contrib import admin
+from models import Product,Category
+from forms import ProductAdminForm
+
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+
+    # sets value for how the admin site lists your products
+    list_display = ('name', 'price', 'old_price', 'created_at', 'updated_at',)
+    list_display_links = ('name',)
+    list_per_page = 50
+    ordering = ['-created_at']
+
+    search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
+#    readonly_fields = ("created_at", "updated_at")
+#    exclude = ('created_at', 'updated_at')
+
+    # sets up slug to be generated from product name
+    prepopulated_fields = {'slug' : ('name',)}
+
+# register product model with the admin site
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    # set up values for how admin site lists categories
+    list_display = ('name', 'created_at', 'updated_at',)
+    list_display_links = ('name', )
+    list_per_page = 20
+    ordering = ['name']
+    search_fields = ['name', 'description', 'meta_keywords', 'meta_description']
+#    readonly_fields = ('created_at','updated_at')
+#    exclude = ('created_at', 'updated_at',)
+
+    # set up slug to be generated from category name
+    prepopulated_fields = {'slug' : ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
--- ecomstore/catalog/forms.py
+++ ecomstore/catalog/forms.py
@@ -1,1 +1,14 @@
 __author__ = 'stefan'
+from django import forms
+from models import Product
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Product
+
+    # validate the price
+    def clean_price(self):
+        # check the value of price
+        if self.cleaned_data['price'] <= 0:
+            raise forms.ValidationError('Price must be greater than zero')
+        return self.cleaned_data['price']
